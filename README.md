f44522d59168ad4563483394068d4d84b3ce2f68
Quinn Michaels <quinnmichaels@gmail.com>
2014-01-23 21:41:52 -0800

    Updated Readme

--- README.md
+++ README.md
@@ -1,16 +1,69 @@
 loadHBS
 =======
 
-#Summary
-loadHBS is a javascript library for injecting handlebars partial templates into a specified layout.
+#SUMMARY:
+	loadJBS is a JavaScript library for loading handlebars templates in to a jQuery selector.
 
-##Example
-See the index.html file for usage and examples.
 
-##Dependencies
--jQuery
--Handlebars
+###AUTHOR:
+	Quinn Michaels
 
-Links to the CDN for both libraries can be found in the index.html file
+
+##REQUIRES:
+	-jQuery				//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js
+	-Handlebars			//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.js
+
+
+##CONSTANTS:
+
+	_path		DEFAULT: ''		ROOT PATH TO HANDLEBARS TEMPLATES
+	_extension	DEFAULT: .hbs	FILE EXTENSION OF YOUR HANDLEBARS TEMPLATES.
+
+***********************************************************************
+Constants can be set at time of loading or set in the conf object:
+***********************************************************************
+###USAGE EXAMPLE:
+```javascript
+loadHBS._path = '/custom/path/';
+loadHBS._extension = '.handlebars';
+loadHBS.template({*conf object*})
+```
+***********************************************************************
+
+##CONF OBJECT:
+
+	type:		insert | append | prepend
+
+	selector: 	SELECTOR OF OBJECT TO INSERT TEMPLATE INTO
+
+	template: 	NAME OF TEMPLATE TO LOAD.  DO NOT INCLUDE THE EXTENSION.
+				THIS METHOD CAN ALSO CALL SUB-DIRECTORIES IE: /views/main OR /views/detail.
+
+	data:		JSON FORMATTED DATA BEING PASSED INTO THE TEMPLATE
+
+	path:		*OPTIONAL		USED TO SET A CUSTOM PATH
+
+	extension:	*OPTIONAL		USED TO SET A CUSTOM EXTENSION
+
+	callback:	CALLBACK FUNCTION TO RUN ONCE THE TEMPLATE HAS LOADED.  THIS IS WHERE
+				YOU INITIATE EVENT HANDLERS THAT MIGHT BE ASSOCIATED WITH A SPECIFIC TEMPLATE.
+
+***********************************************************************
+###USAGE EXAMPLE
+```javascript
+loadHBS.template({
+ 	selector: '%CSS SELECTOR%',
+ 	template: '%TEMPLATE NAME%',
+ 	data: {
+	 	title: '%HANDLEBARS TITLE%',
+	 	text: '%HANDLEBARS TEXT%'
+ 	},
+ 	type: 'insert|append|prepend',
+ 	callback: function() {
+     	// template has loaded and you can inset your event handlers
+ 	}
+});
+```
+***********************************************************************
 
 Copyright 2014 - Quinn Michaels
--- loadHBS.js
+++ loadHBS.js
@@ -1,19 +1,61 @@
 /*
-		CONF OBJECT
+SUMMARY:
+	loadJBS is a JavaScript library for loading handlebars templates in to a jQuery selector.
 
-			type:		append | prepend | insert/default
+AUTHOR:
+	Quinn Michaels
 
-			selector: 	SELECTOR OF OBJECT TO INSERT TEMPLATE INTO
+REQUIRES:
+	-jQuery				//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js
+	-Handlebars			//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.js
 
-			template: 	NAME OF TEMPLATE TO LOAD.  DO NOT INCLUDE THE EXTENSION.
-						THIS METHOD CAN ALSO CALL SUB-DIRECTORIES IE: /views/main OR /views/detail.
+CONSTANTS:
 
-			data:		JSON FORMATTED DATA BEING PASSED INTO THE TEMPLATE
+	_path		DEFAULT: ''		ROOT PATH TO HANDLEBARS TEMPLATES
+	_extension	DEFAULT: .hbs	FILE EXTENSION OF YOUR HANDLEBARS TEMPLATES.
 
+	***********************************************************************
+	Constants can be set at time of loading or set in the conf object:
+	***********************************************************************
+	USAGE EXAMPLE:
+		loadHBS._path = '/custom/path/';
+		loadHBS._extension = '.handlebars';
+		loadHBS.template({*conf object*})
+	***********************************************************************
 
-			callback:	CALLBACK FUNCTION TO RUN ONCE THE TEMPLATE HAS LOADED.  THIS IS WHERE
-						YOU INITIATE EVENT HANDLERS THAT MIGHT BE ASSOCIATED WITH A SPECIFIC TEMPLATE.
+CONF OBJECT:
 
+	type:		insert | append | prepend
+
+	selector: 	SELECTOR OF OBJECT TO INSERT TEMPLATE INTO
+
+	template: 	NAME OF TEMPLATE TO LOAD.  DO NOT INCLUDE THE EXTENSION.
+				THIS METHOD CAN ALSO CALL SUB-DIRECTORIES IE: /views/main OR /views/detail.
+
+	data:		JSON FORMATTED DATA BEING PASSED INTO THE TEMPLATE
+
+	path:		*OPTIONAL		USED TO SET A CUSTOM PATH
+
+	extension:	*OPTIONAL		USED TO SET A CUSTOM EXTENSION
+
+	callback:	CALLBACK FUNCTION TO RUN ONCE THE TEMPLATE HAS LOADED.  THIS IS WHERE
+				YOU INITIATE EVENT HANDLERS THAT MIGHT BE ASSOCIATED WITH A SPECIFIC TEMPLATE.
+
+	***********************************************************************
+	USAGE EXAMPLE
+		loadHBS.template({
+	     	selector: '%CSS SELECTOR%',
+	     	template: '%TEMPLATE NAME%',
+	     	data: {
+			 	title: '%HANDLEBARS TITLE%',
+			 	text: '%HANDLEBARS TEXT%'
+	     	},
+	     	type: 'insert|append|prepend',
+	     	callback: function() {
+		     	// template has loaded and you can inset your event handlers
+	     	}
+		});
+	***********************************************************************
 */
 
 
@@ -21,8 +63,10 @@ var loadHBS = {
 	_path: '',
 	_extension: '.hbs',
 	template: function(conf) {
-		// VALIDATES THE CONF PARAMATER IS AN OBJECT
-		if (typeof conf !== 'object') throw 'Config must be an object';
+		if (typeof conf !== 'object') throw 'Config must be an object';	// validate conf object
+		if (conf.path) this._path = conf.path;							// set custom path
+		if (conf.extension) this._extension = conf.extension;			// set custom extension
+
 
 		$.ajax({
 			url: this._path + conf.template + this._extension,
